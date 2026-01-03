880d71f094394ed60b8e1262b569b1b5fc0cca1a
faisal-gif <fagdera@gmail.com>
2026-01-03 22:40:34 +0700

    refactor: simplify Tripay callback handling by removing payment processing logic

--- app/Http/Controllers/TripayCallbackController.php
+++ app/Http/Controllers/TripayCallbackController.php
@@ -13,44 +13,10 @@ class TripayCallbackController extends Controller
 {
     public function handle(Request $request)
     {
-        $signature = hash_hmac(
-            'sha256',
-            $request->getContent(),
-            config('tripay.private_key')
-        );
-
-        abort_if(
-            $signature !== $request->header('X-Callback-Signature'),
-            403
-        );
-
-        $data = $request->data;
-
-        $payment = Payments::where('reference', $data['reference'])->firstOrFail();
-
-        if ($data['status'] === 'PAID') {
-            DB::transaction(function () use ($payment, $data) {
-                $payment->update([
-                    'status' => 'paid',
-                    'paid_at' => now(),
-                    'payload' => $data,
-                ]);
-
-
-                $newsPackage = NewsPackage::find($payment->package_id);
-
-                $user = User::find($payment->user_id);
-                $user->quota_news += $newsPackage->quota;
-                if ($user->dateexp == null) {
-                    $user->dateexp = now()->addMonth($newsPackage->perioid);
-                } else {
-                    $user->dateexp = $user->dateexp->addMonth($newsPackage->perioid);
-                }
-                $user->package_id = $newsPackage->id;
-                $user->type = $newsPackage->type;
-                $user->save();
-            });
-        }
+        \Log::info('Tripay callback MASUK', [
+            'headers' => $request->headers->all(),
+            'body' => $request->all(),
+        ]);
 
         return response()->json(['success' => true]);
     }
