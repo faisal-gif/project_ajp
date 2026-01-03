09f116f3f2ac2613aea99577d5ecd9dcff09b61f
faisal-gif <fagdera@gmail.com>
2026-01-04 00:00:06 +0700

    fix: update Head component to include meta tags for SEO and social media

--- resources/js/Pages/Welcome/Index.jsx
+++ resources/js/Pages/Welcome/Index.jsx
@@ -9,11 +9,19 @@ import { Head } from '@inertiajs/react';
 
 
 export default function Index({ newsPackages }) {
-   
+
 
     return (
         <>
-            <Head title='Welcome To AJP' />
+            <Head>
+                <title>Beranda</title>
+                <meta name="description" content="AJP (Aplikasi Jurnalisme Positif) adalah platform jurnalistik yang mendorong pemberitaan berimbang, solutif, dan inspiratif untuk membangun optimisme publik." />
+
+                {/* Meta untuk Social Media (Open Graph) */}
+                <meta property="og:title" content="Beranda - AJP" />
+                <meta property="og:description" content="AJP (Aplikasi Jurnalisme Positif) adalah platform jurnalistik yang mendorong pemberitaan berimbang, solutif, dan inspiratif untuk membangun optimisme publik." />
+                <meta property="og:image" content="/logo-web-ajp.png" />
+            </Head>
             <LandingLayout>
                 <HeroSection />
                 <AboutSection />
