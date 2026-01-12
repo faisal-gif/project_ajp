38c59423042ab6501bb47487617cd8d47eb028a6
faisal-gif <fagdera@gmail.com>
2026-01-12 16:41:39 +0700

    modified:   app/Http/Controllers/DashboardController.php 	new file:   public/profile.webp 	modified:   resources/js/Pages/Dashboard.jsx

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,11 +17,9 @@ class DashboardController extends Controller
         $total_news = News::where('pewarta_id', $auth->id)->count();
         $total_pending_news = News::where('pewarta_id', $auth->id)->where('status', 0)->count();
         $total_publish_news = News::where('pewarta_id', $auth->id)->where('status', 1)->count();
-        $paket_terdaftar = NewsPackage::find($auth->package_id);
 
         return Inertia::render('Dashboard', [
             'total_news' => $total_news,
-            'paket_terdaftar' => $paket_terdaftar,
             'pending_news' => $total_pending_news,
             'publish_news' => $total_publish_news,
         ]);
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -4,22 +4,10 @@ import { formatDate } from '@/Utils/formatter';
 import { Head, usePage } from '@inertiajs/react';
 import { AlertTriangle, Badge, BookOpen, Calendar, CheckCircle, Image, MapPin, MousePointer, PenTool, Plus, Send, TrendingUp } from 'lucide-react';
 
-export default function Dashboard({ total_news, paket_terdaftar, pending_news, publish_news }) {
+export default function Dashboard({ total_news,pending_news, publish_news }) {
 
     const { auth } = usePage().props;
     const user = auth.user;
-
-
-    // Calculate days remaining
-    const today = new Date();
-    const endDate = new Date(user.dateexp);
-    const daysRemaining = Math.ceil((endDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
-    const isExpiringSoon = daysRemaining <= 30 && daysRemaining > 0;
-    const isExpired = daysRemaining <= 0;
-
-    const quotaPercentage = (user.quota_news / paket_terdaftar.quota) * 100;
-    const quotaRemaining = paket_terdaftar.quota - user.quota_news;
-    const isQuotaExhausted = user.quota_news <= 0;
 
     return (
         <AuthenticatedLayout
@@ -122,9 +110,6 @@ export default function Dashboard({ total_news, paket_terdaftar, pending_news, p
                                 <Calendar className="w-5 h-5 text-primary" />
                                 Status Membership
                             </div>
-                            <div className='badge badge-outline badge-primary'>
-                                Level {paket_terdaftar.level}
-                            </div>
                         </div>
 
                         <div className="space-y-4 mt-4">
@@ -141,24 +126,6 @@ export default function Dashboard({ total_news, paket_terdaftar, pending_news, p
                                     {formatDate(user.dateexp)}
                                 </span>
                             </div>
-                            <div>
-                                {isExpired ? (
-                                    <div className="flex items-center gap-2 text-primary">
-                                        <AlertTriangle className="w-4 h-4" />
-                                        <span className="font-semibold text-sm">Paket sudah berakhir!</span>
-                                    </div>
-                                ) : isExpiringSoon ? (
-                                    <div className="flex items-center gap-2 text-orange-600">
-                                        <AlertTriangle className="w-4 h-4" />
-                                        <span className="font-semibold text-sm">Sisa {daysRemaining} hari lagi</span>
-                                    </div>
-                                ) : (
-                                    <div className="flex items-center gap-2 text-green-600">
-                                        <CheckCircle className="w-4 h-4" />
-                                        <span className="font-semibold text-sm">Aktif - Sisa {daysRemaining} hari</span>
-                                    </div>
-                                )}
-                            </div>
                         </div>
 
 
@@ -166,34 +133,16 @@ export default function Dashboard({ total_news, paket_terdaftar, pending_news, p
 
                         {/* Kuota Artikel */}
                         <div className="space-y-3">
-                            {paket_terdaftar.level != 1 && (
                                 <div className="flex items-center justify-between">
                                     <p className="text-sm font-medium text-muted-foreground flex items-center gap-2">
-                                        <TrendingUp className={`w-4 h-4 ${isQuotaExhausted ? 'text-destructive' : 'text-primary'}`} />
                                         Kuota Artikel Aktif
                                     </p>
                                     <span className="font-semibold text-foreground text-sm">
-                                        {user.quota_news} / {paket_terdaftar.quota}
+                                        {user.quota_news}
                                     </span>
                                 </div>
-                            )}
                             {/* <Progress value={quotaPercentage} className="h-2" /> */}
-                            {isQuotaExhausted && (
-                                <div className="space-y-3 mt-10">
-                                    <div className="flex items-center gap-2 text-destructive">
-                                        <AlertTriangle className="w-4 h-4" />
-                                        <span className="font-semibold text-sm">Kuota artikel sudah habis!</span>
-                                    </div>
-                                    <Link className="btn btn-outline btn-primary w-full" href={route('subscription.index')} >
-                                        <Crown className="w-4 h-4 mr-2" />
-                                        Perpanjang / Upgrade Member
-                                    </Link>
-                                </div>
-                            )}
 
-                            <p className="text-xs text-muted-foreground">
-                                Sisa {user.quota_news} artikel untuk bulan ini.
-                            </p>
 
                         </div>
                     </Card>
