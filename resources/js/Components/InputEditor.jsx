62238a6af57d9fadf92af3a445a03eb98f0df8a6
faisal-gif <fagdera@gmail.com>
2026-01-14 10:20:11 +0700

    modified:   resources/js/Components/InputEditor.jsx

--- resources/js/Components/InputEditor.jsx
+++ resources/js/Components/InputEditor.jsx
@@ -4,11 +4,9 @@ import { Editor } from '@tinymce/tinymce-react';
 export default function InputEditor({ value, onChange }) {
     return (
         <Editor
-            // 1. GUNAKAN API KEY (Daftar di tiny.cloud untuk dapat gratis)
-            // Atau kosongkan jika ingin pakai mode warning/self-hosted
+
             tinymceScriptSrc="/vendor/tinymce/tinymce.min.js"
             referrerPolicy='origin'
-            // 2. Binding Data
             value={value}
             onEditorChange={(content, editor) => {
                 onChange(content);
