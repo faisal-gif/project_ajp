896d723497b00e8c540935017e6da89d78b8594b
faisal-gif <fagdera@gmail.com>
2026-02-20 15:22:58 +0700

    Changes to be committed: 	modified:   app/Http/Controllers/WartawanController.php
    Changes not staged for commit:
    	modified:   routes/web.php

--- app/Http/Controllers/WartawanController.php
+++ app/Http/Controllers/WartawanController.php
@@ -2,64 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Wartawan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class WartawanController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Wartawan $wartawan)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Wartawan $wartawan)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Wartawan $wartawan)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Wartawan $wartawan)
-    {
-        //
+    public function convert_pswd($id){
+        
+        $user = User::find($id);
+        $user->password = Hash::make($user->passwd);
+        $user->save();
+        
+        return 'berhasil di udpdate';
     }
 }
